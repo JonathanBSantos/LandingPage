47c0fbf1d9e756fd2b704a4a2997afe18cc48769
Jonathan Santos <jonathan.santos.ext@gmail.com>
2023-10-27 10:33:18 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+var SetaEsquerda = window.document.getElementById("Seta-esquerda")
+var Bruna = window.document.getElementById("Bruna")
+var Leonardo = window.document.getElementById("Leonardo")
+var Samantha = window.document.getElementById("Samantha")
+var SetaDireita = window.document.getElementById("Seta-direita")
+
+function RolarParaDireita () {
+    Bruna.style.display = "none"
+    Samantha.style.display = "flex"
+   
+}
+
+
