98daad801af98db4c97a27e7966713466da846a7
Jonathan Santos <jonathan.santos.ext@gmail.com>
2023-10-31 06:06:07 -0300

    versão 2

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ var Samantha = window.document.getElementById("Samantha")
 var SetaDireita = window.document.getElementById("Seta-direita")
 
 function RolarParaDireita () {
-    Bruna.style.display = "none"
-    Samantha.style.display = "flex"
-   
+    Bruna.style = "display:none"
+    Samantha.style = "display:flex"
+    SetaDireita.style = "display:none" 
+    SetaEsquerda.style = "display:flex; margin-top:55px"   
+    }
+
+
+function RolarParaEsquerda () {
+    Leonardo.style = "display:none"
+    Bruna.style = "display:flex"
+    SetaEsquerda.style = "display:=none"
+    SetaDireita.style = "display:flex; margin-top:55px" 
 }
-
-
